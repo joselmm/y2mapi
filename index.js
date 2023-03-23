5eb54f5adc35fc437bccd6cb72bca0dab81e2c4c
joselmm <95531879+joselmm@users.noreply.github.com>
2023-03-22 22:33:25 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const puppeteer = require('puppeteer');
-const browserPromise = puppeteer.launch({headless:true});
+require("dotenv").config();
+const browserPromise = puppeteer.launch({
+    headless:true,
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
 
 let browser = "";
 const port = process.env.PORT || 3000;
